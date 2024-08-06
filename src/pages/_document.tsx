1c512f413b7fb706efb0de40066e846935baae5d
tnrdd <tnrdd@tutanota.com>
2024-08-06 16:48:12 +0200

    feat: add favicons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,22 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon-16x16.png" sizes="16x16" />
+        <link rel="icon" href="/favicon-32x32.png" sizes="32x32" />
+        <link
+          rel="shortcut icon"
+          href="/android-chrome-192x192.png"
+          sizes="192x192"
+        />
+        <link
+          rel="shortcut icon"
+          href="/android-chrome-512x512.png"
+          sizes="512x512"
+        />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
