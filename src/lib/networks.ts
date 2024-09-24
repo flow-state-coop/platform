923562b750b0e996be1a238429e0cf501159c65f
tnrdd <tnrdd@tutanota.com>
2024-09-24 07:32:50 +0200

    chore: update superapp factory address and strategy bytecode for latest fix in contracts

--- src/lib/networks.ts
+++ src/lib/networks.ts
@@ -143,7 +143,7 @@ const networks: Network[] = [
     passportDecoder: "0xe53C60F8069C2f0c3a84F9B3DB5cf56f3100ba56",
     superfluidHost: "0xd399e2Fb5f4cf3722a11F65b88FAB6B2B8621005",
     superfluidResolver: "0x554c06487bEc8c890A0345eb05a5292C1b1017Bd",
-    recipientSuperappFactory: "0xdd023c9c2abf7f2adcedc8be7c688f82f06276de",
+    recipientSuperappFactory: "0x77F9A5D05e37B0c1DAEbB425bf2C69Fa8d9BEF90",
     allo: "0x1133eA7Af70876e64665ecD07C0A0476d09465a1",
     alloRegistry: "0x4AAcca72145e1dF2aeC137E1f3C5E3D75DB8b5f3",
     gdaForwarder: "0x6DA13Bde224A05a288748d857b9e7DDEffd1dE08",
