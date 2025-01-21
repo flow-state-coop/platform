f84f7d29651498499500d46fa3dc0a8d7b729bde
tnrdd <tnrdd@tutanota.com>
2025-01-21 10:06:27 +0100

    fix: long pool card names

--- src/app/flow-splitters/flow-splitters.tsx
+++ src/app/flow-splitters/flow-splitters.tsx
@@ -6,6 +6,7 @@ import { Address, formatEther } from "viem";
 import { useAccount, useWalletClient, useSwitchChain } from "wagmi";
 import { useConnectModal } from "@rainbow-me/rainbowkit";
 import { gql, useQuery, useLazyQuery } from "@apollo/client";
+import { useClampText } from "use-clamp-text";
 import Container from "react-bootstrap/Container";
 import Stack from "react-bootstrap/Stack";
 import Dropdown from "react-bootstrap/Dropdown";
@@ -22,6 +23,10 @@ import { getApolloClient } from "@/lib/apollo";
 import { networks } from "@/lib/networks";
 import { truncateStr } from "@/lib/utils";
 import { SECONDS_IN_MONTH } from "@/lib/constants";
+
+type FlowSplittersProps = {
+  defaultNetwork: Network;
+};
 
 type Pool = {
   id: string;
@@ -76,8 +81,11 @@ const SF_POOL_MEMBERSHIPS_QUERY = gql`
   }
 `;
 
-export default function FlowSplitters() {
-  const [selectedNetwork, setSelectedNetwork] = useState<Network>(networks[1]);
+export default function FlowSplitters(props: FlowSplittersProps) {
+  const { defaultNetwork } = props;
+
+  const [selectedNetwork, setSelectedNetwork] =
+    useState<Network>(defaultNetwork);
   const [pools, setPools] = useState<Pool[]>([]);
 
   const router = useRouter();
@@ -247,6 +255,182 @@ export default function FlowSplitters() {
     });
   };
 
+  const PoolCard = ({ pool, token }: { pool: Pool; token?: Token }) => {
+    const [nameRef, { clampedText }] = useClampText({
+      text:
+        pool.name !== "Superfluid Pool" && pool.symbol !== "POOL"
+          ? `${pool.name} (${pool.symbol})`
+          : pool.name !== "Superfluid Pool"
+            ? `${pool.name}`
+            : pool.symbol !== "POOL"
+              ? `${pool.symbol}`
+              : `${truncateStr(pool.poolAddress, 14)}`,
+      ellipsis: "...",
+      lines: 3,
+    });
+
+    return (
+      <Card
+        className="d-flex flex-col justify-content-center align-items-center border-2 rounded-4 fs-4"
+        style={{ height: 400 }}
+      >
+        <Card.Header className="h-25 bg-white border-0 rounded-4 py-3">
+          <Card.Title
+            ref={nameRef as React.RefObject<HTMLParagraphElement>}
+            className="fs-4 text-center"
+          >
+            {clampedText}
+          </Card.Title>
+        </Card.Header>
+        <Card.Body className="h-50">
+          <Stack
+            direction="horizontal"
+            gap={1}
+            className="justify-content-center mb-3"
+          >
+            <Card.Link
+              href={selectedNetwork.superfluidDashboard}
+              target="_blank"
+              className="d-flex gap-2 align-items-center text-decoration-none"
+            >
+              {token && (
+                <Image src={token.icon} alt="" width={22} height={22} />
+              )}
+              <Card.Text className="text-decoration-underline">
+                {token?.name ?? truncateStr(pool.token, 14)}
+              </Card.Text>
+            </Card.Link>
+            <Button
+              variant="transparent"
+              className="p-0"
+              onClick={() =>
+                !address && openConnectModal
+                  ? openConnectModal()
+                  : connectedChain?.id !== selectedNetwork.id
+                    ? switchChain({ chainId: selectedNetwork.id })
+                    : addToWallet(
+                        token ?? {
+                          address: pool.token as Address,
+                          name: "",
+                          icon: "",
+                        },
+                      )
+              }
+            >
+              <InfoTooltip
+                position={{ top: true }}
+                target={
+                  <Image
+                    width={22}
+                    height={22}
+                    src="/wallet.svg"
+                    alt="wallet"
+                  />
+                }
+                content={<>Add to Wallet</>}
+              />
+            </Button>
+          </Stack>
+          {pool.isAdmin && pool.units === BigInt(0) ? (
+            <Card.Text className="mb-0 fs-2 fw-bold text-center">
+              Admin
+            </Card.Text>
+          ) : (
+            <Stack direction="vertical" gap={1}>
+              {pool.isConnected ? (
+                <Stack
+                  direction="horizontal"
+                  gap={1}
+                  className="justify-content-center"
+                >
+                  <Card.Text className="m-0 fs-2 fw-bold">
+                    {Intl.NumberFormat("en", {
+                      notation: "compact",
+                    }).format(
+                      Number(
+                        formatEther(pool.flowRate * BigInt(SECONDS_IN_MONTH)),
+                      ),
+                    )}
+                  </Card.Text>
+                  <Card.Text className="mt-2 mb-0 fs-6">/mo</Card.Text>
+                </Stack>
+              ) : (
+                <Stack direction="vertical">
+                  <Card.Text className="mb-0 fs-2 fw-bold text-center">
+                    {Number(pool.units)}
+                  </Card.Text>
+                  <Stack
+                    direction="horizontal"
+                    gap={1}
+                    className="align-items-start"
+                  >
+                    <Card.Text className="mb-0 small">
+                      Unconnected Shares
+                    </Card.Text>
+                    <InfoTooltip
+                      position={{ top: true }}
+                      target={
+                        <Image
+                          src="/info.svg"
+                          alt=""
+                          width={16}
+                          height={16}
+                          className="mb-4"
+                        />
+                      }
+                      content={
+                        <>
+                          Complete a one-time transaction to receive your Flow
+                          Splitter stream in real-time. Your funds are safe, but
+                          static until you connect your shares.
+                        </>
+                      }
+                    />
+                  </Stack>
+                </Stack>
+              )}
+              {pool.isAdmin && (
+                <Card.Text className="m-0 fs-6 text-center">Admin</Card.Text>
+              )}
+            </Stack>
+          )}
+        </Card.Body>
+        <Card.Footer className="d-flex flex-column justify-content-end gap-1 w-100 h-25 bg-transparent border-0">
+          {pool.isAdmin && (
+            <Button
+              variant="primary"
+              className="w-100"
+              onClick={() =>
+                router.push(
+                  `/flow-splitters/${selectedNetwork.id}/${pool.id}/admin`,
+                )
+              }
+            >
+              Edit
+            </Button>
+          )}
+          {pool.isConnected ? (
+            <Button
+              variant="secondary"
+              className="w-100"
+              onClick={() =>
+                router.push(`/flow-splitters/${selectedNetwork.id}/${pool.id}`)
+              }
+            >
+              View
+            </Button>
+          ) : (
+            <PoolConnectionButton
+              network={selectedNetwork}
+              poolAddress={pool.poolAddress}
+              isConnected={pool.isConnected}
+            />
+          )}
+        </Card.Footer>
+      </Card>
+    );
+  };
+
   return (
     <Container
       className="mx-auto p-0 mb-5"
@@ -335,12 +519,16 @@ export default function FlowSplitters() {
           >
             <Card
               className="d-flex flex-col justify-content-center align-items-center border-2 rounded-4 fs-4 cursor-pointer"
-              style={{ height: 410 }}
-              onClick={() =>
-                router.push(
-                  `/flow-splitters/launch/?chainId=${selectedNetwork.id}`,
-                )
-              }
+              style={{ height: 400 }}
+              onClick={() => {
+                if (address) {
+                  router.push(
+                    `/flow-splitters/launch/?chainId=${selectedNetwork.id}`,
+                  );
+                } else if (openConnectModal) {
+                  openConnectModal();
+                }
+              }}
             >
               <Image src="/add.svg" alt="add" width={64} />
               <Card.Text className="d-inline-block m-0 overflow-hidden fs-2 text-center word-wrap">
@@ -353,180 +541,7 @@ export default function FlowSplitters() {
                 (token) => token.address.toLowerCase() === pool.token,
               );
 
-              return (
-                <Card
-                  key={i}
-                  className="d-flex flex-col justify-content-center align-items-center border-2 rounded-4 fs-4"
-                  style={{ height: 410 }}
-                >
-                  <Card.Title className="mt-3 fs-3">
-                    {pool.name !== "Superfluid Pool" &&
-                    pool.symbol !== "POOL" ? (
-                      <>
-                        {pool.name} ({pool.symbol})
-                      </>
-                    ) : pool.name !== "Superfluid Pool" ? (
-                      <>{pool.name}</>
-                    ) : pool.symbol !== "POOL" ? (
-                      <>{pool.symbol}</>
-                    ) : (
-                      <>{truncateStr(pool.poolAddress, 14)}</>
-                    )}
-                  </Card.Title>
-                  <Card.Body>
-                    <Stack
-                      direction="horizontal"
-                      className="justify-content-center"
-                    >
-                      <Card.Link
-                        href={selectedNetwork.superfluidDashboard}
-                        target="_blank"
-                        className="d-flex gap-2 align-items-center text-decoration-none"
-                      >
-                        {token && (
-                          <Image
-                            src={token.icon}
-                            alt=""
-                            width={22}
-                            height={22}
-                          />
-                        )}
-                        <Card.Text className="text-decoration-underline">
-                          {token?.name ?? truncateStr(pool.token, 14)}
-                        </Card.Text>
-                      </Card.Link>
-                      <Button
-                        variant="transparent"
-                        onClick={() =>
-                          addToWallet(
-                            token ?? {
-                              address: pool.token as Address,
-                              name: "",
-                              icon: "",
-                            },
-                          )
-                        }
-                      >
-                        <InfoTooltip
-                          position={{ top: true }}
-                          target={
-                            <Image
-                              width={22}
-                              height={22}
-                              src="/wallet.svg"
-                              alt="wallet"
-                            />
-                          }
-                          content={<>Add to Wallet</>}
-                        />
-                      </Button>
-                    </Stack>
-                    {pool.isAdmin && pool.units === BigInt(0) ? (
-                      <Card.Text className="mt-5 mb-0 fs-1 fw-bold text-center">
-                        Admin
-                      </Card.Text>
-                    ) : (
-                      <Stack direction="vertical" gap={1} className="mt-5">
-                        {pool.isConnected ? (
-                          <Stack
-                            direction="horizontal"
-                            gap={1}
-                            className="justify-content-center"
-                          >
-                            <Card.Text className="m-0 fs-1 fw-bold">
-                              {Intl.NumberFormat("en", {
-                                notation: "compact",
-                              }).format(
-                                Number(
-                                  formatEther(
-                                    pool.flowRate * BigInt(SECONDS_IN_MONTH),
-                                  ),
-                                ),
-                              )}
-                            </Card.Text>
-                            <Card.Text className="mt-2 mb-0 fs-6">
-                              /mo
-                            </Card.Text>
-                          </Stack>
-                        ) : (
-                          <Stack direction="vertical">
-                            <Card.Text className="mb-0 fs-1 fw-bold text-center">
-                              {Number(pool.units)}
-                            </Card.Text>
-                            <Stack
-                              direction="horizontal"
-                              gap={1}
-                              className="align-items-start"
-                            >
-                              <Card.Text className="mt-3 mb-0 small">
-                                Unconnected Shares
-                              </Card.Text>
-                              <InfoTooltip
-                                position={{ top: true }}
-                                target={
-                                  <Image
-                                    src="/info.svg"
-                                    alt=""
-                                    width={16}
-                                    height={16}
-                                  />
-                                }
-                                content={
-                                  <>
-                                    Complete a one-time transaction to receive
-                                    your Flow Splitter stream in real-time. Your
-                                    funds are safe, but static until you connect
-                                    your shares.
-                                  </>
-                                }
-                              />
-                            </Stack>
-                          </Stack>
-                        )}
-                        {pool.isAdmin && (
-                          <Card.Text className="m-0 fs-6 text-center">
-                            Admin
-                          </Card.Text>
-                        )}
-                      </Stack>
-                    )}
-                  </Card.Body>
-                  <Card.Footer className="d-flex flex-column gap-1 w-100 bg-transparent border-0">
-                    {pool.isAdmin && (
-                      <Button
-                        variant="primary"
-                        className="w-100"
-                        onClick={() =>
-                          router.push(
-                            `/flow-splitters/${selectedNetwork.id}/${pool.id}/admin`,
-                          )
-                        }
-                      >
-                        Edit
-                      </Button>
-                    )}
-                    {pool.isConnected ? (
-                      <Button
-                        variant="secondary"
-                        className="w-100"
-                        onClick={() =>
-                          router.push(
-                            `/flow-splitters/${selectedNetwork.id}/${pool.id}`,
-                          )
-                        }
-                      >
-                        View
-                      </Button>
-                    ) : (
-                      <PoolConnectionButton
-                        network={selectedNetwork}
-                        poolAddress={pool.poolAddress}
-                        isConnected={pool.isConnected}
-                      />
-                    )}
-                  </Card.Footer>
-                </Card>
-              );
+              return <PoolCard key={i} pool={pool} token={token} />;
             })}
           </div>
         </Stack>
