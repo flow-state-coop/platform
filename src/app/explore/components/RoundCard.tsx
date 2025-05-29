4e88364630fcfe9e6b26e16ff22860c4b45c30c6
tnrdd <tnrdd@tutanota.com>
2025-05-29 17:23:01 +0200

    fix: total funding decimals

--- src/app/explore/components/RoundCard.tsx
+++ src/app/explore/components/RoundCard.tsx
@@ -40,7 +40,7 @@ export default function RoundCard(props: RoundCardProps) {
   return (
     <Card
       className="border-0 rounded-4 shadow cursor-pointer"
-      style={{ width: 380, height: 256 }}
+      style={{ width: 360, height: 256 }}
       onClick={() =>
         totalStreamedUntilUpdatedAt
           ? router.push(link)
@@ -66,7 +66,10 @@ export default function RoundCard(props: RoundCardProps) {
         {totalAmountStreamed ? (
           <>
             <Card.Text className="fs-2 mb-1 fw-bold text-center">
-              {formatNumber(Number(formatEther(totalAmountStreamed)), 6)}{" "}
+              {formatNumber(
+                Number(formatEther(totalAmountStreamed)),
+                tokenSymbol === "ETHX" ? 6 : 4,
+              )}{" "}
               {tokenSymbol}
             </Card.Text>
             <Card.Text className="fs-6 text-center">
