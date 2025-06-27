d949957ea2f9dbfa4408f6012751f72a64c144ff
Graven Prest <gravenp@protonmail.com>
2025-06-27 07:54:18 -0600

    fix: adds new g$ council address

--- src/app/gooddollar/lib/councilConfig.ts
+++ src/app/gooddollar/lib/councilConfig.ts
@@ -1,4 +1,4 @@
 export const councilConfig: { [key: string]: { councilAddress: string } } = {
-  [42220]: { councilAddress: "0x3573af721603c67a2742d768718a52a40f8b6021" },
+  [42220]: { councilAddress: "0x553a2f9341fb3e2ee9de0b70e1019331d3d77971" },
   [11155420]: { councilAddress: "0xf1bd7df8beea17f6f81a153b85e788390e684213" },
 };
