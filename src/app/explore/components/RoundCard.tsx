ba9821f9879f1e6cf3679eb16d5dd5292c02faa6
tnrdd <tnrdd@tutanota.com>
2025-08-04 21:04:04 +0200

    fix: open link in new tab

--- src/app/explore/components/RoundCard.tsx
+++ src/app/explore/components/RoundCard.tsx
@@ -1,4 +1,4 @@
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 import { formatEther } from "viem";
 import Card from "react-bootstrap/Card";
 import Image from "react-bootstrap/Image";
@@ -29,8 +29,6 @@ export default function RoundCard(props: RoundCardProps) {
     updatedAt,
   } = props;
 
-  const router = useRouter();
-
   const totalAmountStreamed = useFlowingAmount(
     BigInt(totalStreamedUntilUpdatedAt ?? 0),
     updatedAt ?? 0,
@@ -38,60 +36,61 @@ export default function RoundCard(props: RoundCardProps) {
   );
 
   return (
-    <Card
-      className="border-0 rounded-4 shadow cursor-pointer"
-      style={{ width: 360, height: 256 }}
-      onClick={() =>
-        totalStreamedUntilUpdatedAt
-          ? router.push(link)
-          : window.open(link, "_blank")
-      }
+    <Link
+      href={link}
+      target={link.startsWith("https://farcaster") ? "_blank" : "_self"}
+      className="text-decoration-none"
     >
-      <Card.Header className="position-relative bg-transparent border-0 p-3">
-        <Stack direction="horizontal" className="align-items-center">
-          <Image
-            src={image}
-            alt=""
-            width={38}
-            height={38}
-            className="position-absolute start-0 ms-2 rounded-3"
-          />
-          <Card.Title className="w-100 mb-1 fs-5 fw-bold text-center">
-            {name}
-          </Card.Title>
-        </Stack>
-        <Card.Text className="text-center fs-6">{roundType}</Card.Text>
-      </Card.Header>
-      <Card.Body>
-        {totalAmountStreamed ? (
-          <>
-            <Card.Text className="fs-2 mb-1 fw-bold text-center">
-              {formatNumber(
-                Number(formatEther(totalAmountStreamed)),
-                tokenSymbol === "ETHx" ? 6 : 4,
-              )}{" "}
-              {tokenSymbol}
+      <Card
+        className="border-0 rounded-4 shadow cursor-pointer"
+        style={{ width: 360, height: 256 }}
+      >
+        <Card.Header className="position-relative bg-transparent border-0 p-3">
+          <Stack direction="horizontal" className="align-items-center">
+            <Image
+              src={image}
+              alt=""
+              width={38}
+              height={38}
+              className="position-absolute start-0 ms-2 rounded-3"
+            />
+            <Card.Title className="w-100 mb-1 fs-5 fw-bold text-center">
+              {name}
+            </Card.Title>
+          </Stack>
+          <Card.Text className="text-center fs-6">{roundType}</Card.Text>
+        </Card.Header>
+        <Card.Body>
+          {totalAmountStreamed ? (
+            <>
+              <Card.Text className="fs-2 mb-1 fw-bold text-center">
+                {formatNumber(
+                  Number(formatEther(totalAmountStreamed)),
+                  tokenSymbol === "ETHx" ? 6 : 4,
+                )}{" "}
+                {tokenSymbol}
+              </Card.Text>
+              <Card.Text className="fs-6 text-center">
+                Total Funding Streamed
+              </Card.Text>
+            </>
+          ) : (
+            <Card.Text className="fs-2 fw-bold text-center">
+              Coming Soon
             </Card.Text>
-            <Card.Text className="fs-6 text-center">
-              Total Funding Streamed
-            </Card.Text>
-          </>
-        ) : (
-          <Card.Text className="fs-2 fw-bold text-center">
-            Coming Soon
-          </Card.Text>
-        )}
-      </Card.Body>
-      <Card.Footer className="bg-transparent border-0">
-        <Stack
-          direction="horizontal"
-          gap={2}
-          className="justify-content-end fw-bold"
-        >
-          <Image src="/sup.svg" alt="" width={32} height={32} />
-          Rewards
-        </Stack>
-      </Card.Footer>
-    </Card>
+          )}
+        </Card.Body>
+        <Card.Footer className="bg-transparent border-0">
+          <Stack
+            direction="horizontal"
+            gap={2}
+            className="justify-content-end fw-bold"
+          >
+            <Image src="/sup.svg" alt="" width={32} height={32} />
+            Rewards
+          </Stack>
+        </Card.Footer>
+      </Card>
+    </Link>
   );
 }
