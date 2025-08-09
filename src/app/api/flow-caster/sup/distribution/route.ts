2dfd82e74350734bf338e2a6115130bb6a7d6257
tnrdd <tnrdd@tutanota.com>
2025-08-09 08:57:34 +0200

    feat: add endpoint to trigger flow caster SUP points update on events

---
+++ src/app/api/flow-caster/sup/distribution/route.ts
@@ -0,0 +1,153 @@
+import { NextRequest } from "next/server";
+import { getAddress, formatEther } from "viem";
+import { gql, request } from "graphql-request";
+import { StackClient } from "@stackso/js-core";
+import { networks } from "@/lib/networks";
+import { GDAPool } from "@/types/gdaPool";
+
+export const dynamic = "force-dynamic";
+
+const GDA_POOL_QUERY = gql`
+  query GDAPoolQuery($gdaPool: String!) {
+    pool(id: $gdaPool) {
+      poolDistributors(first: 1000) {
+        account {
+          id
+        }
+        flowRate
+        totalAmountFlowedDistributedUntilUpdatedAt
+        updatedAtTimestamp
+      }
+    }
+  }
+`;
+
+const PROGRAM_ID = 7743;
+const POOLS = [
+  "0x9ef9fe8bf503b10698322e3a135c0fa6decc5b5b",
+  "0x6719cbb70d0faa041f1056542af66066e3cc7a24",
+];
+
+const getDistributors = async (subgraphEndpoint: string, timestamp: number) => {
+  const distributors: {
+    address: string;
+    totalDistributed: bigint;
+    hasStreamToTeamPool: boolean;
+  }[] = [];
+
+  for (const poolId of POOLS) {
+    const queryRes = await request<{ pool: GDAPool }>(
+      subgraphEndpoint,
+      GDA_POOL_QUERY,
+      {
+        gdaPool: poolId,
+      },
+    );
+
+    for (const distributor of queryRes.pool.poolDistributors) {
+      const existingDistributor = distributors.find(
+        (x) => x.address === distributor.account.id,
+      );
+      const totalDistributed =
+        BigInt(distributor.totalAmountFlowedDistributedUntilUpdatedAt) +
+        BigInt(distributor.flowRate) *
+          BigInt(timestamp - distributor.updatedAtTimestamp);
+
+      const hasStreamToTeamPool =
+        poolId === POOLS[1] && distributor.flowRate !== "0";
+
+      if (existingDistributor) {
+        existingDistributor.totalDistributed += totalDistributed;
+        existingDistributor.hasStreamToTeamPool = hasStreamToTeamPool;
+      } else {
+        distributors.push({
+          address: distributor.account.id,
+          totalDistributed,
+          hasStreamToTeamPool,
+        });
+      }
+    }
+  }
+
+  return distributors;
+};
+
+export async function GET(req: NextRequest) {
+  try {
+    const chainId = req.nextUrl.searchParams.get("chainId") ?? 8453;
+    const network = networks.find((network) => network.id === Number(chainId));
+
+    if (!network) {
+      return new Response(
+        JSON.stringify({ success: false, error: "Wrong network" }),
+      );
+    }
+
+    const tokensPerDollar = 1;
+    const now = (Date.now() / 1000) | 0;
+    const stack = new StackClient({
+      apiKey:
+        network.id === 8453
+          ? process.env.STACK_API_KEY_FLOW_CASTER!
+          : process.env.STACK_API_KEY_OP_SEPOLIA!,
+      pointSystemId: PROGRAM_ID,
+    });
+    const distributors = await getDistributors(network.superfluidSubgraph, now);
+    const events = [];
+
+    if (distributors) {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const currentPointsAll: any = await stack.getPoints(
+        distributors.map((x) => getAddress(x.address)),
+        {
+          event: "distributed",
+        },
+      );
+
+      for (const distributor of distributors) {
+        const currentPoints =
+          currentPointsAll?.find(
+            (x: { address: string }) =>
+              x.address.toLowerCase() === distributor.address,
+          )?.amount ?? 0;
+        const totalDistributed = distributor.totalDistributed;
+        const newPoints =
+          (Number(formatEther(totalDistributed)) / tokensPerDollar) | 0;
+        const diff = newPoints - currentPoints;
+
+        if (diff > 0) {
+          events.push({
+            event: "distributed",
+            payload: {
+              points: distributor.hasStreamToTeamPool ? diff * 2 : diff,
+              account: distributor.address,
+              uniqueId: `${distributor.address}-${now}`,
+            },
+          });
+        }
+      }
+    }
+
+    if (events.length > 0) {
+      await stack.trackMany(events);
+
+      return new Response(
+        JSON.stringify({
+          success: true,
+          message: "Points updated",
+        }),
+      );
+    }
+
+    return new Response(
+      JSON.stringify({
+        success: false,
+        error: "Nothing to update",
+      }),
+    );
+  } catch (err) {
+    console.error(err);
+
+    return new Response(JSON.stringify({ success: false, error: err }));
+  }
+}
--- src/app/api/flow-caster/sup/route.ts
+++ src/app/api/flow-caster/sup/route.ts
@@ -1,153 +1,66 @@
-import { NextRequest } from "next/server";
-import { getAddress, formatEther } from "viem";
-import { gql, request } from "graphql-request";
 import { StackClient } from "@stackso/js-core";
 import { networks } from "@/lib/networks";
-import { GDAPool } from "@/types/gdaPool";
 
 export const dynamic = "force-dynamic";
 
-const GDA_POOL_QUERY = gql`
-  query GDAPoolQuery($gdaPool: String!) {
-    pool(id: $gdaPool) {
-      poolDistributors(first: 1000) {
-        account {
-          id
-        }
-        flowRate
-        totalAmountFlowedDistributedUntilUpdatedAt
-        updatedAtTimestamp
-      }
-    }
-  }
-`;
-
-const PROGRAM_ID = 7743;
-const POOLS = [
-  "0x9ef9fe8bf503b10698322e3a135c0fa6decc5b5b",
-  "0x6719cbb70d0faa041f1056542af66066e3cc7a24",
-];
-
-const getDistributors = async (subgraphEndpoint: string, timestamp: number) => {
-  const distributors: {
-    address: string;
-    totalDistributed: bigint;
-    hasStreamToTeamPool: boolean;
-  }[] = [];
-
-  for (const poolId of POOLS) {
-    const queryRes = await request<{ pool: GDAPool }>(
-      subgraphEndpoint,
-      GDA_POOL_QUERY,
-      {
-        gdaPool: poolId,
-      },
-    );
-
-    for (const distributor of queryRes.pool.poolDistributors) {
-      const existingDistributor = distributors.find(
-        (x) => x.address === distributor.account.id,
-      );
-      const totalDistributed =
-        BigInt(distributor.totalAmountFlowedDistributedUntilUpdatedAt) +
-        BigInt(distributor.flowRate) *
-          BigInt(timestamp - distributor.updatedAtTimestamp);
-
-      const hasStreamToTeamPool =
-        poolId === POOLS[1] && distributor.flowRate !== "0";
-
-      if (existingDistributor) {
-        existingDistributor.totalDistributed += totalDistributed;
-        existingDistributor.hasStreamToTeamPool = hasStreamToTeamPool;
-      } else {
-        distributors.push({
-          address: distributor.account.id,
-          totalDistributed,
-          hasStreamToTeamPool,
-        });
-      }
-    }
-  }
-
-  return distributors;
-};
-
-export async function GET(req: NextRequest) {
+export async function POST(req: Request) {
   try {
-    const chainId = req.nextUrl.searchParams.get("chainId") ?? 8453;
-    const network = networks.find((network) => network.id === Number(chainId));
+    const authHeader = req.headers.get("authorization");
+
+    if (authHeader !== `Bearer ${process.env.FLOW_CASTER_SUP_ACCESS_TOKEN}`) {
+      return new Response(
+        JSON.stringify({ success: false, error: "Unauthorized" }),
+        { status: 401 },
+      );
+    }
+
+    const { chainId, event, account, points } = await req.json();
+
+    const network = networks.find(
+      (network) => network.id === Number(chainId ?? 8453),
+    );
 
     if (!network) {
       return new Response(
         JSON.stringify({ success: false, error: "Wrong network" }),
+        { status: 401 },
       );
     }
 
-    const tokensPerDollar = 1;
     const now = (Date.now() / 1000) | 0;
     const stack = new StackClient({
       apiKey:
         network.id === 8453
           ? process.env.STACK_API_KEY_FLOW_CASTER!
           : process.env.STACK_API_KEY_OP_SEPOLIA!,
-      pointSystemId: PROGRAM_ID,
+      pointSystemId: network.id === 8453 ? 7743 : 7717,
     });
-    const distributors = await getDistributors(network.superfluidSubgraph, now);
-    const events = [];
 
-    if (distributors) {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const currentPointsAll: any = await stack.getPoints(
-        distributors.map((x) => getAddress(x.address)),
-        {
-          event: "distributed",
-        },
-      );
-
-      for (const distributor of distributors) {
-        const currentPoints =
-          currentPointsAll?.find(
-            (x: { address: string }) =>
-              x.address.toLowerCase() === distributor.address,
-          )?.amount ?? 0;
-        const totalDistributed = distributor.totalDistributed;
-        const newPoints =
-          (Number(formatEther(totalDistributed)) / tokensPerDollar) | 0;
-        const diff = newPoints - currentPoints;
-
-        if (diff > 0) {
-          events.push({
-            event: "distributed",
-            payload: {
-              points: distributor.hasStreamToTeamPool ? diff * 2 : diff,
-              account: distributor.address,
-              uniqueId: `${distributor.address}-${now}`,
-            },
-          });
-        }
-      }
-    }
-
-    if (events.length > 0) {
-      await stack.trackMany(events);
-
-      return new Response(
-        JSON.stringify({
-          success: true,
-          message: "Points updated",
-        }),
-      );
-    }
+    await stack.track(event, {
+      points,
+      account,
+      uniqueId: `${account}-${now}`,
+    });
 
     return new Response(
       JSON.stringify({
-        success: false,
-        error: "Nothing to update",
+        success: true,
+        message: "Points updated",
       }),
     );
   } catch (err) {
     console.error(err);
 
-    return new Response(JSON.stringify({ success: false, error: err }));
+    if (err instanceof Error) {
+      return new Response(
+        JSON.stringify({ success: false, error: err.message }),
+        { status: 400 },
+      );
+    } else {
+      return new Response(
+        JSON.stringify({ success: false, error: "Unkown Error" }),
+        { status: 400 },
+      );
+    }
   }
 }
