02ed3cc65700c7b5a39829dc6b3524fb947e527f
Graven Prest <15019279+gravenp@users.noreply.github.com>
2026-02-11 11:49:36 -0700

    uses shorter placeholder text

--- src/app/flow-councils/components/chat/MessageInput.tsx
+++ src/app/flow-councils/components/chat/MessageInput.tsx
@@ -21,7 +21,7 @@ export default function MessageInput(props: MessageInputProps) {
     isSending,
     showEmailCheckbox = false,
     disabled = false,
-    placeholder = "Write your message... Markdown syntax is supported.",
+    placeholder = "Write a message. Markdown is supported.",
   } = props;
 
   const [content, setContent] = useState("");
