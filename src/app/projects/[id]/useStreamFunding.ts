8e2724250f78e0b2cc840f774bcb876c07b1966d
tnrdd <tnrdd@tutanota.com>
2026-03-22 11:54:06 +0100

    fix: use legacy transaction executor in stream funding

--- src/app/projects/[id]/useStreamFunding.ts
+++ src/app/projects/[id]/useStreamFunding.ts
@@ -90,7 +90,7 @@ export default function useStreamFunding(
     areTransactionsLoading,
     completedTransactions,
     transactionError,
-    executeTransactions,
+    executeLegacyTransactions: executeTransactions,
   } = useTransactionsQueue();
 
   const isCorrectChain = walletChainId === network.id;
